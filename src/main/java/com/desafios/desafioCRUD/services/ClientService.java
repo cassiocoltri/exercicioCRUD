393127a4e912eaca23cfe3fbebf08468f313027e
Cássio Coltri <94485441+cassiocoltri@users.noreply.github.com>
2023-07-21 15:30:30 -0300

    Primeiro teste da API via Postman
    Primeiro teste 200 ok

--- src/main/java/com/desafios/desafioCRUD/services/ClientService.java
+++ src/main/java/com/desafios/desafioCRUD/services/ClientService.java
@@ -1,8 +1,31 @@
 package com.desafios.desafioCRUD.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desafios.desafioCRUD.entities.Client;
+import com.desafios.desafioCRUD.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Optional<Client> findById(Long id) {
+		Optional<Client> client = repository.findById(id);
+		return client;
+	}
+	
+	@Transactional(readOnly = true)
+	public Page <Client> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result;
+	}
 }
